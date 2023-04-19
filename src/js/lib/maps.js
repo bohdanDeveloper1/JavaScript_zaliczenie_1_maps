6ddf7a47ed5fcdfac76273ff60f62784246673fc
Jakub Bilski <jakub@blsk.pl>
2023-04-19 09:36:48 +0200

    Sample map implementation.

--- src/js/lib/maps.js
+++ src/js/lib/maps.js
@@ -1,3 +1,16 @@
+import L from 'leaflet';
+import _ from 'leaflet-gesture-handling';
+
 export default () => {
-    console.log('Hello maps!');
+    const mapEl = document.getElementById('map');
+
+    const map = L.map(mapEl, {
+        center: [54.4526626, 17.0398293],
+        zoom: 13,
+        gestureHandling: true
+    });
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
 }
