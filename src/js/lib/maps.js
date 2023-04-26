1e65282bc55572f3e480ca3f9799c3549e15d51b
Jakub Bilski <jakub@blsk.pl>
2023-04-26 08:55:12 +0200

    Moved leaflet to be loaded as dynamic JS chunk using promises.

--- src/js/lib/maps.js
+++ src/js/lib/maps.js
@@ -1,16 +1,22 @@
-import L from 'leaflet';
-import _ from 'leaflet-gesture-handling';
-
 export default () => {
     const mapEl = document.getElementById('map');
-
-    const map = L.map(mapEl, {
-        center: [54.4526626, 17.0398293],
-        zoom: 13,
-        gestureHandling: true
-    });
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19,
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    }).addTo(map);
+    if (mapEl) {
+        import('leaflet').then((L) => {
+            import('leaflet-gesture-handling').then(() => {
+                const map = L.map(mapEl, {
+                    center: [54.4526626, 17.0398293],
+                    zoom: 13,
+                    gestureHandling: true
+                });
+                L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                    maxZoom: 19,
+                    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+                }).addTo(map);
+            }).catch((error) => {
+                console.log(error);
+            });
+        }).catch((error) => {
+            console.log(error);
+        });
+    }
 }
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,7 @@
 let mix = require('laravel-mix');
 
+mix.setPublicPath('public');
+
 
 mix.js('src/js/index.js', 'public/js/app.js');
 
