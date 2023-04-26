b2cfe46a9c26e458957d73ca1c3ad1d81c99af37
Jakub Bilski <jakub@blsk.pl>
2023-04-26 09:00:06 +0200

    Changed importing leaflet from promises to async await.

--- src/js/lib/maps.js
+++ src/js/lib/maps.js
@@ -1,22 +1,21 @@
-export default () => {
+export default async () => {
     const mapEl = document.getElementById('map');
     if (mapEl) {
-        import('leaflet').then((L) => {
-            import('leaflet-gesture-handling').then(() => {
-                const map = L.map(mapEl, {
-                    center: [54.4526626, 17.0398293],
-                    zoom: 13,
-                    gestureHandling: true
-                });
-                L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                    maxZoom: 19,
-                    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-                }).addTo(map);
-            }).catch((error) => {
-                console.log(error);
+        try {
+            const L = await import('leaflet');
+            await import('leaflet-gesture-handling');
+
+            const map = L.map(mapEl, {
+                center: [54.4526626, 17.0398293],
+                zoom: 13,
+                gestureHandling: true
             });
-        }).catch((error) => {
+            L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                maxZoom: 19,
+                attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+            }).addTo(map);
+        } catch(error) {
             console.log(error);
-        });
+        }
     }
-}
+};
